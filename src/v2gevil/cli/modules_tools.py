6a52e32ff088f86d1f201c2fc5ddfe20189a6330
khuntpav <p.smoulin@seznam.cz>
2023-11-15 20:55:42 +0100

    Add fuzzer methods to modules tools

--- src/v2gevil/cli/modules_tools.py
+++ src/v2gevil/cli/modules_tools.py
@@ -11,6 +11,8 @@ import rich_click as click
 from ..enumerator import enumerate_ev as EV_enumerator
 from ..enumerator.enumerator_enums import EVEnumMode
 from ..fuzzer.fuzzer import EVFuzzer
+from ..fuzzer.fuzzer_enums import EVFuzzMode
+from ..station.station_enums import EVSEChargingMode, EVSEDetails
 
 
 @click.group()
@@ -23,7 +25,7 @@ def modules_tools():
 @click.option(
     "--interface",
     "-i",
-    default="eth_station",
+    default=EVSEDetails.INTERFACE.value,
     show_default=True,
     help="Interface to run station on",
 )
@@ -53,8 +55,119 @@ def enumerate_ev(interface: str, enum_mode: str):
 
 # Modules for fuzzing
 @modules_tools.command(name="fuzz-EV")
-def fuzz_ev():
+@click.option(
+    "--interface",
+    "-i",
+    default=EVSEDetails.INTERFACE.value,
+    show_default=True,
+    help="Interface to run station on",
+)
+@click.option(
+    "--mode",
+    "-m",
+    default="all",
+    show_default=True,
+    help="""
+        Mode of fuzzing. Possible values: all, custom, message, config.\n
+        all fuzz all messages and all possible params\n
+        custom: fuzz only specified params (specified by user)\n
+        message: fuzz only specified message (specified by user)
+        Message name must be specified by --message-name option.\n
+        config: fuzz only messages and params specified in fuzzer config file.
+        """,
+)
+@click.option(
+    "--message-name",
+    default="",
+    show_default=True,
+    help="""Name of message to fuzz. Only used in message mode.
+        Fuzzed message will be only specified message not others.""",
+)
+@click.option(
+    "--charging-mode",
+    "-cm",
+    default="AC",
+    show_default=True,
+    help="Charging mode of EVSE. Possible values: AC, DC.",
+)
+@click.option(
+    "--custom-dict",
+    "custom_dict_filename",
+    default=None,
+    show_default=True,
+    help="Path to file with custom dictionary, which will be used for fuzzing.",
+)
+@click.option(
+    "--config-filename",
+    "config_filename",
+    default="ev_fuzzer_config_default.toml",
+    show_default=True,
+    help="""Name of the fuzzer config file.
+    File must be in the config directory of fuzzer module.
+    Option is applicable only for config mode or message mode.
+    For message mode: attributes for fuzzing for the specified message
+    will be taken from the config file.""",
+)
+def fuzz_ev(
+    interface: str,
+    mode: str,
+    message_name: str,
+    charging_mode: str,
+    custom_dict_filename: str,
+    config_filename: str,
+):
     """Fuzz EV"""
-    # TODO: Implement
+    try:
+        mode = EVFuzzMode(mode)
+    except ValueError:
+        print(f"Invalid fuzz mode: {mode}")
+        return
+    try:
+        charging_mode = EVSEChargingMode(charging_mode)
+    except ValueError:
+        print(f"Invalid charging mode: {charging_mode}")
+        return
+
+    ev_fuzzer = EVFuzzer(
+        interface=interface,
+        mode=mode,
+        charging_mode=charging_mode,
+        custom_dict_filename=custom_dict_filename,
+        config_filename=config_filename,
+    )
+    # TODO: Add custom dict support, only for the function not for the class
+
+    # Fuzz
+    # Depending on mode, different function will be called
+    # message_name is only used in message mode
+    # Only chosen message will be fuzzed, specified by message_name, in message mode
+    ev_fuzzer.fuzz(message_name=message_name)
+
+
+@modules_tools.command(name="create-EV-custom-dict")
+@click.option(
+    "--charging-mode",
+    "-cm",
+    default="AC",
+    show_default=True,
+    help="Charging mode of EVSE. Possible values: AC, DC.",
+)
+@click.option(
+    "--filename",
+    "-fn",
+    default="",
+    show_default=True,
+    help="The name of the file to which the dictionary is to be saved",
+)
+# TODO: Add params like messages names and relevant params
+# TODO: Create some method like create custom fuzzing dict
+# TODO: ASK TOM, Do you think that this is a good idea?
+# because user can modify the dict in text editor
+# so i think in this case it will be good to not take care about values for params
+# but only about message name and relevant params
+def create_ev_fuzzing_dict(charging_mode: str, filename: str):
+    """Create custom fuzzing dict"""
+    # TODO: Check if file exist and inform user, if he wants to overwrite it
+    # flag for overwriting
+    exit()
     raise NotImplementedError
-    ev_fuzzer = EVFuzzer()
