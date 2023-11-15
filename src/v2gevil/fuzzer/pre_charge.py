9b0e52d83717c3014a163586b52dc4c7d1bd49af
khuntpav <p.smoulin@seznam.cz>
2023-11-15 20:08:10 +0100

    Fuzzer Class for Pre Charge Response message

---
+++ src/v2gevil/fuzzer/pre_charge.py
@@ -0,0 +1,55 @@
+"""Fuzzer classes for Pre Charge request and response messages."""
+
+from typing import Optional
+
+from .fuzz_params import (
+    fuzz_response_code,
+    fuzz_dc_evse_status,
+    fuzz_evse_present_voltage,
+)
+from .fuzz_msg_general import general_msg_fuzzing_method
+
+
+class FuzzerPreChargeRes:
+    """Fuzzer class for Cable Check response message"""
+
+    def __init__(
+        self,
+        msg_config: Optional[dict] = None,
+        msg_fuzz_dict: Optional[dict] = None,
+        msg_default_dict: Optional[dict] = None,
+    ):
+        self.msg_config = msg_config
+        self.msg_fuzz_dict = msg_fuzz_dict
+        self.msg_default_dict = msg_default_dict
+
+    def fuzz(
+        self,
+    ) -> dict:
+        """Fuzz the message"""
+
+        # Pairs of parameter/field name and fuzzing method
+        pairs_name_method = {
+            "ResponseCode": fuzz_response_code,
+            "DC_EVSEStatus": fuzz_dc_evse_status,
+            "EVSEPresentVoltage": fuzz_evse_present_voltage,
+        }
+        # Required fields define in the standard
+        required_fields = [
+            "ResponseCode",
+            "DC_EVSEStatus",
+            "EVSEPresentVoltage",
+        ]
+
+        # All possible fields (required/optional) define in the standard
+        all_fields = ["ResponseCode", "DC_EVSEStatus", "EVSEPresentVoltage"]
+
+        return general_msg_fuzzing_method(
+            required_fields=required_fields,
+            all_fields=all_fields,
+            msg_config=self.msg_config,
+            msg_fuzz_dict=self.msg_fuzz_dict,
+            msg_default_dict=self.msg_default_dict,
+            pairs_name_method=pairs_name_method,
+            class_name=self.__class__.__name__,
+        )
