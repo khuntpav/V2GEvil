52d2ac8cfa867d53cdd099f25258f5a0712fad2b
khuntpav <p.smoulin@seznam.cz>
2024-01-10 17:28:47 +0100

    Add script for build and deploy this tool

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -3,5 +3,5 @@
 poetry build
 # Copy content of dist folder to ~/V2G/bin/V2GEvil/
 cp ~/V2G/repos/V2GEvil/dist/* ~/V2G/bin/V2GEvil/
-sudo pip3 install ~/V2G/bin/V2GEvil/*.whl
+sudo pip3 install ~/V2G/bin/V2GEvil/v2gevil-1.0.0*.whl
 sudo v2gevil --help
