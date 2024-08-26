4c2964d54a336b70ba8c694a8d6d56c960bba6f2
V2GEvil <p.smoulin@seznam.cz>
2024-08-26 15:25:00 +0200

    Adding license/copyright

--- src/v2gevil/v2gtp/console_handler.py
+++ src/v2gevil/v2gtp/console_handler.py
@@ -1,4 +1,19 @@
 """
+V2GEvil - Tool for testing and evaluation of V2G communication.
+Copyright (C) 2024 Pavel Khunt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+"""
+
+"""
 Console handler for V2GTP
 
 This module contains methods for handling printing to console.
